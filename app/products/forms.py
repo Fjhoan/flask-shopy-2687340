3751c9cd4b0b03a9344cda4f940bb61fa13f94ad
lauravat <lau3232435124@gmail.com>
2023-09-06 12:14:03 -0500

    clase 6-09-2023

--- app/products/forms.py
+++ app/products/forms.py
@@ -1,8 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
+from flask_wtf.file import FileField
+from wtforms.validators import InputRequired, NumberRange
+from flask_wtf.file import FileAllowed, FileRequired
 
 #Formulario de registro de nuevo producto
 class NewProductForm(FlaskForm):
-    nombre = StringField("Ingresa el nombre: ")
-    precio = StringField("Ingresa el precio: ")
+    nombre = StringField(validators=[ InputRequired(message="Falta el nombre") ],
+                        label="Ingresa el nombre: ")
+    precio = IntegerField(label="Ingresa el precio: ", 
+                        validators=[ InputRequired(message="precio requerido"),
+                                    NumberRange(message="precio fuera de rango", min= 1000, max= 10000)
+                                    ])
+    imagen = FileField(label="cargue imagen del producto:",
+                        validators=[
+                            FileRequired(
+                                message="Suba una imagen"
+                            ),
+                            FileAllowed(
+                                ["jpg","png"],
+                                message="Tipo de archivo incorrecto"
+                            )
+                        ])
     submit = SubmitField("Registrar")
